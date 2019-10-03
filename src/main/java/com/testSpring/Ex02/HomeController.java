c7b58933ed04d0c7578f947f57d9368944d31aba
dewytear <youngjinkwak@gmail.com>
2019-10-03 18:11:00 +0900

    20191003 spring4 ex02

---
+++ src/main/java/com/testSpring/Ex02/HomeController.java
@@ -0,0 +1,59 @@
+package com.testSpring.Ex02;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate );
+
+		return "home";
+	}//home()
+
+	@RequestMapping("member/test1")
+	public String test(HttpServletRequest httpServletRequest, Model model) {
+		String company = httpServletRequest.getParameter("company");
+		String group = httpServletRequest.getParameter("group");
+		String id = httpServletRequest.getParameter("id");
+		String pw = httpServletRequest.getParameter("pw");
+
+		model.addAttribute("company", company);
+		model.addAttribute("group", group);
+		model.addAttribute("id", id);
+		model.addAttribute("pw", pw);
+
+		//아래 URL처럼 파라메터 전달하여 확인
+		//http://localhost:9090/Ex02/member/test1?company=test&group=test&id=dewytear&pw=1234567
+
+		return "member/test1";
+	}//test()
+
+}
